68f9349b0f6173162405ed0593cb771940b38ba0
Sumelt <40728261+Sumelt@users.noreply.github.com>
2018-10-10 23:46:27 +0800

    10/10

---
+++ PAT甲code/1009.cpp
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description:
+
+* @Creath Date: 06/10/18 17:46
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	
+}
+
+
+
+
