98540bcd74c98834706531a0d12f27d9ff1d72ac
Sumelt <40728261+Sumelt@users.noreply.github.com>
2018-10-28 21:21:14 +0800

    10/28
    依旧热爱；还有很长的路要走

--- My coring idea/DFS_邻接矩阵.cpp
+++ My coring idea/DFS_邻接矩阵.cpp
@@ -2,7 +2,7 @@
 
 * @Author: Su
 
-* @Description:
+* @Description: 枚举所有点，找到一条路径 
 
 * @Creath Date:
 
@@ -10,16 +10,46 @@
 
 #include <iostream>
 #include <algorithm>
-#include <string>
-#include <set>
-#include <map>
-#include <string>
 
 using namespace std;
+const int MAXV = 1000;
+const int INF = 1000000000;
+
+int n, G[MAXV][MAXV] = {(0)};
+bool vis[MAXV] = {false};
+
+void DFS(int i, int deep)
+{
+	vis[i] = true;
+	
+	for(int j = 0; j<n; j++)
+	{
+		if(vis[j]==false&&G[i][j]!=0)  //访问下一个节点 
+			{
+				printf("both of them: %d %d have connect\n",i, j);
+				DFS(j, deep+1);
+			}			
+	}
+}
+
+void DFSTrave()
+{
+	for(int i = 0; i<n; i++)
+		if(!vis[i])  //该点还没访问
+			DFS(i, 1); 
+}
 
 int main(int argc, char *argv[])
 {
-
+	n = 3;
+	G[0][1] = 1;
+	G[1][0] = 1;
+	G[1][2] = 1; 
+	G[2][1] = 1;
+	G[0][2] = 1;
+	G[2][0] = 1;
+	
+	DFSTrave();
 	return 0;
 }
 
