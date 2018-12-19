d58fc13e249051f5f2dd5499f2f713a2726557d1
Sumelt <40728261+Sumelt@users.noreply.github.com>
2018-12-19 18:32:56 +0800

    12/19

---
+++ 候捷C++11/component.h
@@ -0,0 +1,32 @@
+#include<vector>
+using namespace std;
+
+class Primitive;
+class Component;
+class Composite;
+
+class Component
+{
+	int value;
+	public:
+		Component(int val){
+			value = val;
+		}
+		virtual void add(Component*){}
+};
+
+class Primitive : public Component
+{
+	public:
+		Primitive(int val) : Component(val){}
+};
+
+class Composite : public Component
+{
+	vector<Component*>c;
+	public:
+		Composite(int val) : Component(val){}
+		void add(Component* elem){
+			c.push_back(elem);
+		}
+};
