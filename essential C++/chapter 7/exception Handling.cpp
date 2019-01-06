27c725cfe82307b074c2294cb4d727f66a348f91
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-01-06 21:52:19 +0800

    2019/01/06

---
+++ essential C++/chapter 7/exception Handling.cpp
@@ -0,0 +1,66 @@
+/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description:
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+
+using namespace std;
+static bool flag = 0;
+
+class iterator_overflow{
+	public:
+		iterator_overflow( int index, int max ) 
+			: _index( index ), _max( max ){}
+		int index(){ return _index; }
+		int max() { return _max; }
+		void what_happen( ostream &os = cerr ){
+			os <<"throw exception" <<endl;
+		}
+				
+	private:
+		int _index;
+		int _max;
+};
+
+bool myexception( bool flag )
+{
+	if( flag )
+		throw iterator_overflow( 3, 2 );
+	else return true;
+}
+
+int myexception_handing()
+{
+	int erro_number = 0;
+//	myexception( true );
+//	cout <<"no print message!" <<endl;
+	try{
+		myexception( true );
+		cout <<"no print message!" <<endl;
+	}	 
+	catch( iterator_overflow &iof ){
+		iof.what_happen( cout );
+		erro_number = 40;
+	}
+	catch( ... ){
+		cout <<"Catch all exception!" <<endl;
+	}
+	
+	return erro_number;
+}
+
+int main(int argc, char *argv[])
+{
+	myexception_handing();
+	return 0;
+}
+
+
+
+
