28524f7dfc3ccf63fed9f38b904108c7730f8d24
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-01-27 22:24:51 +0800

    2019/01/27

---
+++ Primer c++/7.1.cpp
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description:
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+bool flag; 
+int main(int argc, char *argv[])
+{
+	vector<int>vec;
+	int i;
+	int a[i];
+	//cout <<(flag);
+	return 0;
+}
+
+
+
+
