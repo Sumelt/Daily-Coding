ee72a3d8322eee2dea8a936120ab133f46f60577
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-02-14 21:12:36 +0800

    2019/02/14

---
+++ 数据结构/树/哈夫曼树操作集.cpp
@@ -0,0 +1,132 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date: 
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int MaxSize = 50; //堆的最大容量 
+int MinDate = -1; //堆的哨兵 */
+
+typedef struct TreeNode *HuffmanTree;
+typedef struct heap *MinHeap;
+typedef int *Heap;
+
+struct TreeNode{
+	int weight; //权值
+	HuffmanTree lchild, rchild;
+	TreeNode(int value) : lchild(NULL), rchild(NULL), weight(value){}
+
+};
+
+struct heap{
+	vector< HuffmanTree > heapArray;
+	int ArraySize;  //当前堆中的元素个数 
+};
+
+
+void InsertMinHeap( MinHeap &H, HuffmanTree &FT )
+{
+	int i = ++H->ArraySize;
+	for( ; H->heapArray[ i/2 ]->weight > FT->weight; i/=2 )
+		H->heapArray[ i ] = H->heapArray[ i/2 ];
+	H->heapArray[ i ] = FT;
+	
+}
+
+
+void downAdjust( MinHeap &hmin, int i )
+{
+	int minIndx = i*2;
+	for( ; minIndx <= hmin->ArraySize; minIndx = i*2 ) //拿到 I的孩子节点下标 
+	{
+		if( minIndx+1 <= hmin->ArraySize && 
+				hmin->heapArray[ minIndx ]->weight > hmin->heapArray[ minIndx+1 ]->weight )
+			{
+				minIndx = minIndx + 1; 
+			}
+		if( hmin->heapArray[ i ]->weight > hmin->heapArray[ minIndx ]->weight )
+		{
+			swap( hmin->heapArray[ i ], hmin->heapArray[ minIndx ] );
+			i =  minIndx; //更新 I
+		}
+			
+		else break;
+	}
+}
+
+
+MinHeap BuildMinHeap( Heap array, int n)
+{
+	MinHeap minHeap = new heap;
+	minHeap->heapArray.push_back( new TreeNode( MinDate ) );
+	minHeap->ArraySize = n;
+	for( int i = 0; i < n; ++i )
+		minHeap->heapArray.push_back( new TreeNode( array[ i ] ) );
+	for( int i = 1; i <= minHeap->ArraySize; ++i )
+	{
+		downAdjust( minHeap, i );
+	}
+		
+	return minHeap;
+}
+
+
+HuffmanTree DeleteMinHeapTop( MinHeap &H )
+{
+	TreeNode *temp = H->heapArray[ 1 ];
+	swap( H->heapArray[ 1 ], H->heapArray[ H->ArraySize ] );
+	--H->ArraySize;
+	downAdjust( H, 1 );
+
+	return temp;
+}
+
+
+HuffmanTree CreatHuffmanTree( Heap array, int n )
+{
+	int i;
+	HuffmanTree FT;
+	MinHeap minHeap = BuildMinHeap( array, n ); //构建最小堆 
+		
+	for( int i = 1; i < n; ++i )
+	{
+	 	FT = new TreeNode(0);
+	 	FT->lchild = DeleteMinHeapTop( minHeap );
+	 	FT->rchild = DeleteMinHeapTop( minHeap );
+	 	FT->weight = FT->lchild->weight + FT->rchild->weight;
+	 	InsertMinHeap( minHeap, FT );
+	}
+	FT = DeleteMinHeapTop( minHeap );
+
+	return FT;
+}
+
+
+void Print( HuffmanTree &FT )
+{
+	if( FT == NULL ) return;
+	else
+	{
+		cout << FT->weight << " ";
+		Print( FT->lchild );
+		Print( FT->rchild );
+	}
+}
+
+
+int main(int argc, char *argv[])
+{
+	int array[] = {3,1,2,5,4};
+	HuffmanTree FT = CreatHuffmanTree( array, sizeof( array )/sizeof( *array ) );
+	Print( FT );
+	return 0;
+}
+
--- 数据结构/树/堆操作集.cpp
+++ 数据结构/树/堆操作集.cpp
@@ -27,28 +27,12 @@ struct heap{
 };
 
 
-//堆调整
+//堆调整 向下 调整 
 void downAdjust( heap *&hx, int indx) 
 {
-	int maxChildIndx = indx * 2; //存左孩子下标 
-	/*while( maxChildIndx <= hx->ArraySize ) //存在左孩子 
-	{
-		if( maxChildIndx + 1 <= hx->ArraySize &&  //存在右孩子 且右孩子的权值大于左孩子权值 
-			( hx->heapArray[ maxChildIndx+1 ] > hx->heapArray[ maxChildIndx ]) )
-		{
-			maxChildIndx = maxChildIndx + 1;
-		} 
-		if(  hx->heapArray[ indx ] < hx->heapArray[ maxChildIndx ] ) //该点的权值比它最大孩子的权值还小 
-		{
-			swap( hx->heapArray[ indx ], hx->heapArray[ maxChildIndx ] );//交换权值 权值较大上去 
-			indx = maxChildIndx; //拿到下标 判断该位置合不合适 
-			maxChildIndx = indx * 2;
-		}
-		else break; //找到合适的点 
-			 
-	}*/
+	//int maxChildIndx = indx * 2; //存左孩子下标 
 	
-	for( ; maxChildIndx <= hx->ArraySize; maxChildIndx = indx * 2 )
+	for(int maxChildIndx = indx * 2; maxChildIndx <= hx->ArraySize; maxChildIndx = indx * 2 )
 	{
 		if( maxChildIndx + 1 <= hx->ArraySize &&  //存在右孩子 且右孩子的权值大于左孩子权值 
 			( hx->heapArray[ maxChildIndx+1 ] > hx->heapArray[ maxChildIndx ]) )
@@ -59,9 +43,8 @@ void downAdjust( heap *&hx, int indx)
 		{
 			swap( hx->heapArray[ indx ], hx->heapArray[ maxChildIndx ] );//交换权值 权值较大上去 
 			indx = maxChildIndx; //拿到下标 判断该位置合不合适 
-
 		}
-		else break;
+		else break; //合适的位置 
 	}
 	
 }
@@ -100,16 +83,48 @@ Heap CreateMaxHeap( int array[], int N )
 		
 }
 
+//堆排序（最小堆） 
+void heapSortUp( heap *&hx )
+{
+	int temp = hx->ArraySize;
+	for( int i = temp; i > 1; --i )
+	{
+		swap( hx->heapArray[ 1 ], hx->heapArray[ i ] );
+		--hx->ArraySize;
+		downAdjust( hx, 1 );
+	}
+	hx->ArraySize = temp;
+}
+
+//堆排序（最大堆） 
+void heapSortDown( heap *&hx )
+{
+	int temp = hx->ArraySize;
+	for( int i = temp, j = 1; i > j; --i, ++j ) // i之前的都排好了 j之后的都排好 i > j就不必排了 
+	{
+		swap( hx->heapArray[ j ], hx->heapArray[ i ] );
+		--hx->ArraySize;
+		downAdjust( hx, j );
+	}
+	hx->ArraySize = temp;
+}
+
 int main()
 {
 	int array[] = {5, 1, 8, 0, 3, 6};
-	heap *hp = CreateMaxHeap(array, sizeof( array )/sizeof( *array ));	
-	//deleteTop( hp );
-	insert( hp, 7 );
+	heap *hp = CreateMaxHeap(array, sizeof( array )/sizeof( *array ));
 	for( int i = 1; i <= hp->ArraySize; ++i ) 
-	{
 		cout << hp->heapArray[ i ];
-	}
+	cout << endl;
+	//deleteTop( hp );
+	//insert( hp, 7 );
+	heapSortUp( hp );
+	for( int i = 1; i <= hp->ArraySize; ++i ) 
+		cout << hp->heapArray[ i ];
+	cout << endl;
+	heapSortDown( hp );
+	for( int i = 1; i <= hp->ArraySize; ++i ) 
+		cout << hp->heapArray[ i ];
 	
 	return 0;
 }
