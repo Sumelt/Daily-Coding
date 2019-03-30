fd23eb78f86448e50c9380ee2695459ceba46492
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-03-30 22:08:36 +0800

    2019/03/30

---
+++ Primer c++/18.cpp
@@ -0,0 +1,29 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date: 
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include "18.h"
+
+namespace space{
+	int value;
+}
+
+namespace space{
+	char c = 3;
+}
+
+namespace sp = space;
+
+int main(int argc, char *argv[])
+{
+	sp::value = 3;
+	sp::c = 4;
+	return 0;
+}
---
+++ Primer c++/18.h
@@ -0,0 +1,9 @@
+﻿#ifndef _18_H
+#define _18_H
+
+
+
+int value;
+
+#endif
+
