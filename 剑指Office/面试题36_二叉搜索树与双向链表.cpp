0a3b342aa99d9a52d3aa536559951e2d350f4cbc
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-03-31 21:58:43 +0800

    2019/03/31

--- 剑指Office/面试题36_二叉搜索树与双向链表.cpp
+++ 剑指Office/面试题36_二叉搜索树与双向链表.cpp
@@ -9,7 +9,53 @@
 ----------------------------------------------------------------*/
 
 #include <iostream>
+#include <vector>
 using namespace std;
+
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};
+
+class Solution {
+private:
+	void ConvertNode( TreeNode *curNode, TreeNode *&connectNode )//这里必须是指针的指针
+	{
+		if( curNode == nullptr )
+			return;
+		
+		if( curNode->left != nullptr )
+			ConvertNode( curNode->left, connectNode );//不断遍历到左子树的叶节点
+		curNode->left = connectNode;//该节点的左指针指向要连接的节点，也就是双向链表中该节点的前指针
+		
+		if( connectNode != nullptr )//连接节点不为空情况下
+			connectNode->right = curNode;//连接节点的右指针指向当前的节点，即是双向链表中该节点的后指针
+		connectNode = curNode;//要连接节点上移，变成当前的节点
+		
+		if( curNode->right != nullptr )
+			ConvertNode( curNode->right, connectNode );//当前节点下移，递归右子树
+	}
+public:
+    TreeNode* Convert(TreeNode* pRootOfTree)
+    {
+        if( pRootOfTree == nullptr )
+        	return nullptr;
+       	TreeNode *connectNode = nullptr;
+       	ConvertNode( pRootOfTree, connectNode );//调用之后connectNode称为尾节点
+       	TreeNode *listHead =  connectNode;//指向尾节点
+       	
+       	while( listHead != nullptr && listHead->left != nullptr ) //寻找链表的头节点
+       		listHead = listHead->left;//不断往前找
+      		
+  		return listHead;
+    }
+};
+
+
 int main(int argc, char *argv[])
 {
 	
---
+++ 剑指Office/面试题39_数组中出现次数超过一半的数字.cpp
@@ -0,0 +1,108 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date: 
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+private:
+	bool checkTime( vector<int> &numbers, int aimNumber ){
+		int sumTime = 0;
+		for( int i = 0; i < numbers.size(); ++i ) //检查该数字出现的次数
+			if( numbers[ i ] == aimNumber )
+				++sumTime;
+		if( sumTime * 2 > numbers.size() )//出现次数过半的2倍一定大于数组长度
+			return true;
+		else return false;
+	}
+public:
+    int MoreThanHalfNum_Solution(vector<int> numbers) {
+    	if( numbers.size() == 0 )
+    		return 0;
+   		int moreTimeNum = 0;//保存数值
+   		int time = 0;//保存该数值的次数
+   		
+   		for( int i = 0; i < numbers.size(); ++i ) {
+			if( time == 0 ) { //次数为0则保存当前的数字
+				time = 1;//并置次数为1
+				moreTimeNum = numbers[ i ];	
+			}
+			else if( moreTimeNum == numbers[ i ] )//它们相同
+				++time;
+			else --time;//它们不同
+	   }
+        //出现次数过半的数字，一定是最后一次把次数设置为1的数字
+		if( time >= 1 && checkTime( numbers, moreTimeNum ) )
+			return moreTimeNum;
+		else return 0;
+    }
+};
+
+class Solution2 {
+private:
+	int getKeyIndex( vector<int> &numbers, int leftIndex, int rightIndex ) {
+		int keyIndex = leftIndex;
+		int keyValue = numbers[ keyIndex ];
+		
+		while( leftIndex < rightIndex ) {
+			while( leftIndex < rightIndex && numbers[ rightIndex ] >= keyValue )
+				--rightIndex;
+			while( leftIndex < rightIndex && numbers[ leftIndex ] <= keyValue )
+				++leftIndex;
+			if( leftIndex < rightIndex )
+				swap( numbers[ leftIndex ], numbers[ rightIndex ] );
+			else break;
+		}
+		swap( numbers[ leftIndex ], numbers[ keyIndex ] );
+		return leftIndex;
+	}
+	bool checkTime( vector<int> &numbers, int aimNumber ){
+		int sumTime = 0;
+		for( int i = 0; i < numbers.size(); ++i ) //检查该数字出现的次数
+			if( numbers[ i ] == aimNumber )
+				++sumTime;
+		
+		if( sumTime * 2 > numbers.size() )//出现次数过半的2倍一定大于数组长度
+			return true;
+		else false;
+	}
+public:
+    int MoreThanHalfNum_Solution(vector<int> numbers) {
+    	if( numbers.size() == 0 )
+    		return 0;
+   		int midIndex = numbers.size() >> 1;
+   		int keyIndex = 0, leftIndex = 0, rightIndex = numbers.size() - 1;
+   		
+   		while( true	) {
+			keyIndex = getKeyIndex( numbers, leftIndex, rightIndex );
+			if( keyIndex == midIndex )
+				break;
+			
+			if( keyIndex > midIndex )
+				rightIndex = keyIndex - 1;
+			else leftIndex = keyIndex + 1;
+			
+	   	}
+	   	
+   		if( checkTime( numbers, numbers[ midIndex ] ) )
+   			return numbers[ midIndex ];
+		else return 0;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+	Solution2 oj;
+	cout << oj.MoreThanHalfNum_Solution( {
+		4,2,1,4,2,4
+	} );
+	return 0;
+}
