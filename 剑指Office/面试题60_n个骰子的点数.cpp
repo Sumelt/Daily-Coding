238a82eb66e88ea3b5d1f2f7b1e3299cc509e127
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-04-06 23:59:07 +0800

    2019/04/06

---
+++ 剑指Office/面试题60_n个骰子的点数.cpp
@@ -0,0 +1,20 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date: 
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+
+
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
