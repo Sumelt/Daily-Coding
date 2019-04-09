d2e82d5cc23d6d6a68d9ebf4eedc5a7095b8dd9f
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-04-09 23:17:58 +0800

    2019/04/09

--- 剑指Office/面试题60_n个骰子的点数.cpp
+++ 剑指Office/面试题60_n个骰子的点数.cpp
@@ -9,12 +9,92 @@
 ----------------------------------------------------------------*/
 
 #include <iostream>
+#include <cstdio>
+#include <cmath>
 using namespace std;
 
-
-
+const int maxPoints = 6; //骰子最大点数，便于扩展
+class Solution {
+private:
+	int maxSum;
+	int minSum;
+	
+	int *array = nullptr;
+	void Split( int sumCount ) {
+		for( int point = 1; point <= maxPoints; ++point )
+			SplitCal( sumCount, sumCount, point );//第一个骰子的点数
+	}
+	void SplitCal( int sumCount, int curCount, int reSum ) {//骰子总数 当前剩余骰子个数 累加和
+		if( curCount == 1 ) { //该堆只有一个骰子
+			++array[ reSum - sumCount ];
+			return;
+		}			
+		for( int point = 1; point <= maxPoints; ++point )
+			SplitCal( sumCount, curCount - 1, reSum + point );//不断分成两堆
+	}
+public:
+    void printProbability( int sumCount ) {
+        if( sumCount == 0 )
+        	return;
+       	maxSum = sumCount * maxPoints;
+       	minSum = sumCount;
+       	array = new int[ maxSum - minSum + 1 ]();//申请空间
+       	double total = pow( maxPoints, sumCount );//排列组合情况总数
+       	
+       	Split( sumCount );
+       	
+       	for( int i = minSum; i <= maxSum; ++i )
+       		printf( "%d : %e\n", i, array[ i - sumCount ]/total );
+  		delete []array;      	
+    }
+};
+/* bug 
+class Solution2 {
+private:
+	int maxSum;
+	int minSum;
+	int flag = 0;
+	int *array[2] = { nullptr, nullptr };
+	void calProbability( int sumCount ) {
+		
+		for( int i = 1; i <= maxPoints; ++i )
+			array[ flag ][ i ] = 1;
+		
+		for( int k = 2; k <= sumCount; ++k ) {
+			for( int i = 0; i < k; ++i )
+				array[ 1 - flag ][ i ] = 0;
+			
+			for( int i = k; i <= maxPoints * k; ++i ) {
+				array[ 1- flag ][ i ] = 0;
+				for( int j = 1; j <= i && j <= maxPoints; ++j )
+					array[ 1- flag ][ j ] += array[ flag ][ i - j ];
+			}
+			flag = 1 - flag;		
+		}		 
+	}
+public:
+    void printProbability( int sumCount ) {
+        if( sumCount == 0 )
+        	return;
+       	minSum = sumCount;
+       	maxSum = maxPoints * sumCount;
+       	array[ 0 ] = new int[ maxSum + 1 ]();
+       	array[ 1 ] = new int[ maxSum + 1 ]();
+       	
+       	calProbability( sumCount );
+       	double total = pow( maxPoints, sumCount );
+       	for( int i = minSum; i <= maxSum * sumCount; ++i )
+       		//printf( "%d : %e\n", i, array[ flag ][ i ]/total );
+ 			cout << array[ flag ][ i ] << endl;
+  		delete []array[ 0 ];
+		delete []array[ 1 ];      	
+    }
+};
+*/
 int main(int argc, char *argv[])
 {
+	Solution2 oj;
+	oj.printProbability( 2 );
 	
 	return 0;
 }
--- 剑指Office/面试题66_构建乘积数组.cpp
+++ 剑指Office/面试题66_构建乘积数组.cpp
@@ -9,7 +9,27 @@
 ----------------------------------------------------------------*/
 
 #include <iostream>
+#include <vector>
 using namespace std;
+
+void arrayProduct( const vector<int>vec1, vector<int>vec2 )
+{
+	int size1 = vec1.size();
+	int size2 = vec2.size();
+	
+	if( size2 > 1 && size1 == size2 ) { //给定的空间相等并且空间大小至少为2		
+        //自上而下
+        vec2[ 0 ] = 1;
+		for( int i = 1; i < size2; ++i )
+			vec2[ i ] = vec2[ i - 1 ] * vec1[ i - 1 ];
+		
+        //自下而上
+		vec2[ size2 - 1 ] = 1;
+		for( int i = size2 - 2; i >= 0; --i )
+			vec2[ i ] *= vec2[ i + 1 ] * vec1[ i + 1 ];
+	}	
+}
+
 int main(int argc, char *argv[])
 {
 	
