e768567ff74ff975f17ebcb819ba5f78b310fe32
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-04-10 23:53:55 +0800

    2019/04/10

---
+++ Primer c++/10_4.cpp
@@ -0,0 +1,26 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date: 
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(int argc, char *argv[])
+{
+	vector<int>vec = { 1, 2, 3 };
+	vector<int>vec2;
+
+	//copy( vec.cbegin(), vec.cend(), inserter( vec2, vec2.begin() ) );	
+	//cout << vec2.front();
+	
+	back_insert_iterator< vector<int> > it =  back_inserter( vec );
+	*it = 5;
+	cout << vec.back();
+	return 0;
+}
---
+++ Primer c++/8.cpp
@@ -0,0 +1,28 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date: 
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <sstream> 
+#include <fstream> 
+using namespace std;
+int main(int argc, char *argv[])
+{
+//	string line;
+//	getline( cin, line );
+//	istringstream record( line );
+//	string str;
+//	while( record >> str ) 
+//		cout << str;
+//	ostringstream outreord;
+//	outreord << 2;
+//	record.tie( &outreord );
+	
+	return 0;
+}
