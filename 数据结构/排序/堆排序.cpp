fbae3698d4556eff1db39a25a741f2812c800926
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-04-23 22:46:26 +0800

    2019/04/23

--- 数据结构/排序/堆排序.cpp
+++ 数据结构/排序/堆排序.cpp
@@ -25,36 +25,28 @@ void StackSort( priority_queue<char, vector<char>,
 			
 	}
 }
-
+//堆的类 
 namespace heapSortVersion2 {
 class maxHeap{
 private:
 	int *data = nullptr;
-	int sumCount = 0;
+	int capacity = 0;
 	int curCount = 0;
-		void shiftDown( int index ) {
-		
-		int tempValue = data[ index ];//当前节点元素值
-		int leftIndex = index * 2;
-		int rigthIndex = leftIndex + 1;
-        
-		while( leftIndex <= curCount ) {//存在左节点
-			if( rigthIndex <= curCount ) { //存在右节点
-				int maxIndex = maxValueIndex( leftIndex, rigthIndex );//返回左右节点中值最大的下标
-				data[ index ] = data[ maxIndex ];//最大元素上移
-				index = maxIndex;//当前节点下移
-			}
-			else if( data[ index ] < data[ leftIndex ] ) {
-				data[ index ] = data[ leftIndex ];
-				index = leftIndex;
-			}
-			else break;//找到合适的位置
-            //更新当前节点后也要同步更新左右子节点
-			leftIndex = index * 2;
-			rigthIndex = leftIndex + 1;				
+
+	void shiftDown( int index ) {
+		int tempValue = data[ index ];
+		while( 2 * index <= curCount ) {
+			int leftIndex = 2 * index;
+			if( leftIndex + 1 <= curCount && data[ leftIndex + 1 ] > data[ leftIndex ] )
+				++leftIndex;
+			if( tempValue >= data[ leftIndex ] )
+				break;
+			else data[ index ] = data[ leftIndex ];
+			index = leftIndex;
 		}
 		data[ index ] = tempValue;
 	}
+		
 	void shiftUp( int index ) {
 		
 		int tempValue = data[ index ];//暂存当前的要调整的元素值
@@ -64,21 +56,17 @@ private:
 		}
 		data[ index ] = tempValue;//正确的位置
 	}
-	int maxValueIndex( int leftIndex, int rigthIndex ) {
-		
-		return data[ leftIndex ] > data[ rigthIndex ] ? leftIndex : rigthIndex;
-	}
 
 public:
 	maxHeap( int length ) {
 	
-		sumCount = length;	
+		capacity = length;	
 		data = new int[ length + 1 ]();		
 	}
 	//heapify
 	maxHeap( int *array, int length ) {
 		
-		sumCount = length;
+		capacity = length;
 		curCount = length;
 		data = new int[ length + 1 ]();
 		copy( array, array + length, data + 1 );
@@ -94,27 +82,30 @@ public:
 	int size() {
 		return curCount;
 	}
+	//删除堆顶元素
 	int popMax() {
-		
-		int res = data[ 1 ];
-		data[ 1 ] = data[ curCount ];
-		--curCount;
-		shiftDown( 1 );
-		return res;
+	    int res = data[ 1 ];//保存堆顶元素
+	    data[ 1 ] = data[ curCount ];//尾元素成为新堆顶
+	    --curCount;
+	    shiftDown( 1 );//开始向下调整
+	    
+	    return res;
 	}
 	
 	void insert( int number ) {
-		
-		assert( curCount <= sumCount );
-		data[ ++curCount ] = number;
-		shiftUp( curCount );		
+			
+	    assert( curCount <= capacity );//断言保证容量足够
+	    data[ curCount + 1 ] = number;
+	    shiftUp( curCount + 1 );
+	    ++curCount;
 	}
 
 	~maxHeap() {
 		delete []data;
 	}
- };
+};
 
+//堆元素一个个插入 
 void maxHeadSort1( int *array, int length ) {
 	maxHeap heap = maxHeap( length );
 	
@@ -124,25 +115,58 @@ void maxHeadSort1( int *array, int length ) {
 	for( int i = length - 1; i >= 0; --i )
 		array[ i ] = heap.popMax();
 }
-
+//直接把数组调整成最大堆 
 void maxHeadSort2( int *array, int length ) {
 	maxHeap heap = maxHeap( array, length );
 	
-	for( int i = 0; i < length; ++i )
+	//从小到大排列 
+	for( int i = length - 1; i >= 0; --i )
 		array[ i ] = heap.popMax();
-	}
 
+	}
 }
+
+namespace heapSortVersion3 {
+//原地堆排序，不开辟额外空间	
+void _shiftDown( int *array, int index, int length ) {
 	
+	int tempValue = array[ index ];
+	int leftIndex = index * 2 + 1;
+	
+	while( leftIndex < length ) {
+		//找出左右节点最大的下标
+        if( leftIndex + 1 < length && array[ leftIndex + 1 ] > array[ leftIndex ] ) 
+			++leftIndex;
+		//利用暂存元素和左右节点中最大的进行比较
+        if( tempValue >= array[ leftIndex ] ) //暂存节还是最大，则表示此位置是正确的则跳出
+			break;
+		else array[ index ] = array[ leftIndex ];//否则最大元素上移
+		index = leftIndex;//暂存元素下标下移
+		leftIndex = index * 2 + 1;//重新更新左节点的下标
+	}
+	array[ index ] = tempValue;
+}
+
+//数组下标从0开始存储 ，返回从小到大的元素序列 
+void maxHeapSort( int *array, int length ) {
+		for( int i = ( length - 2 ) / 2; i >= 0; --i )
+			_shiftDown( array, i, length ); //把数组原地排成最大堆
+			
+		for( int i = length -  1; i > 0; --i ) {
+			swap( array[ 0 ], array[ i ] );//把最大元素放到数组的尾部，形成数组从小到大排列
+			_shiftDown( array, 0, i );//重新调整堆顶，向下调整
+		}			
+	}
+}
 
 int main(int argc, char *argv[])
 {
-	using namespace heapSortVersion2;
+	using namespace heapSortVersion3;
 	
-	int length = 20;
+	int length = 100;
 	int *data = Generate_random_numbers( 1, 100, length );
-	maxHeadSort1( data, length );
 	
+	testSort( "heapsort", maxHeapSort, data, length );
 	for( int i = 0; i < length; ++i )
 		cout << data[ i ] << ends;
 	
