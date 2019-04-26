6aeb55ab0835596d402ba82ab27a394f98c0e7e3
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-04-26 23:57:38 +0800

    2019/04/26

--- 剑指Office/面试题10_矩形覆盖.cpp
+++ 剑指Office/面试题10_矩形覆盖.cpp
@@ -9,7 +9,28 @@
 ----------------------------------------------------------------*/
 
 #include <iostream>
+#include <vector>
 using namespace std;
+
+//动态规划 自底向上
+class Solution {
+public:
+int rectCover( int n ) {
+	vector<int>memo( n + 1, -1 ); //保存计算所得的结果
+	memo[ 0 ] = 0;
+	memo[ 1 ] = 1;
+	memo[ 2 ] = 2;
+	if( n < 3 )
+		return memo[ n ];
+	for( int i = 3; i <= n; ++i )
+		memo[ i ] = memo[ i - 1 ] + memo[ i - 2 ];
+	
+	return memo[ n ];
+	}
+};
+
+
+
 int main(int argc, char *argv[])
 {
 	
--- 数据结构/排序/基数排序.cpp
+++ 数据结构/排序/基数排序.cpp
@@ -9,16 +9,75 @@
 ----------------------------------------------------------------*/
 
 #include <iostream>
-#include <
+#include "../random_number.h"
 using namespace std;
 
-template <typename T>
-void sort( T *arr, int length ) {
-			
+//template <typename T>
+//void sort( T *arr, int length ) {
+//			
+//}
+
+int getMax( int *arr, int n ) {
+	int max = arr[ 0 ];
+	for( int i = 1; i < n; ++i )
+		if( arr[ i ] > max )
+			max = arr[ i ];
+	return max;
 }
+
+void countSort( int *arr, int n, int exp ) {
+	int outPut[ n ];
+	int i, count[ 10 ] = { 0 };
+	
+	for( int i = 0; i < n; ++i ) {
+		count[ ( arr[ i ]/exp )%10 ]++;
+		//cout << "count[ ( arr[ i ]/exp )%10 ]: " << count[ ( arr[ i ]/exp )%10 ] << endl;
+	}
+	cout << "one for" << endl;
+		
+	for( i = 1; i < 10; ++i ) {
+		count[ i ] +=count[ i - 1 ];
+		//cout << "count[ i ] +=count[ i - 1 ]]: " << count[ i ] << endl;
+	}
+	//cout << "two for" << endl;	
+		
+	for( i = n - 1; i >= 0; --i ) {
+		cout << "before outPut[ count[ ( arr[ i ] / exp ) % 10 ] - 1 ]: " << outPut[ count[ ( arr[ i ] / exp ) % 10 ] - 1 ] << endl;
+		outPut[ count[ ( arr[ i ] / exp ) % 10 ] - 1 ] = arr[ i ];
+		cout << "outPut[ count[ ( arr[ i ] / exp ) % 10 ] - 1 ]: " << outPut[ count[ ( arr[ i ] / exp ) % 10 ] - 1 ] << endl;
+		count[ ( arr[ i ] / exp )%10 ]--;
+		//cout << "count[ ( arr[ i ] / exp )%10 ]--: " << count[ ( arr[ i ] / exp )%10 ] << endl;
+	}
+	//cout << "three for" << endl;
+	
+	for( i = 0; i < n; ++i ) {
+		arr[ i ] = outPut[ i ];
+		//cout << "arr[ i ]" << arr[ i ] << ends;
+	}
+	//cout << "end for" << endl;	
+}
+
+void radixSort( int *arr, int n ) {
+	int m = getMax( arr, n );
+	for( int exp = 1; m/exp > 0; exp *= 10 )
+		countSort( arr, n, exp );
+}
+
 
 int main(int argc, char *argv[])
 {
+	int length = 30;
+	int *data = Generate_random_numbers( 20, 100, length );
+	
+	for( int i = 0; i < length; ++i )
+		cout << data[ i ] << ends;
+	cout << endl;
+	
+	radixSort( data, length );
+	
+	for( int i = 0; i < length; ++i )
+		cout << data[ i ] << ends;
+	delete []data;
 	
 	return 0;
 }
