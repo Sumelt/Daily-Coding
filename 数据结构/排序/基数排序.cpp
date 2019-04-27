2594865e4856a5831fc4d4ef856e154ede101b7f
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-04-27 23:41:04 +0800

    2019/04/27

--- 数据结构/排序/基数排序.cpp
+++ 数据结构/排序/基数排序.cpp
@@ -9,74 +9,56 @@
 ----------------------------------------------------------------*/
 
 #include <iostream>
+#include <vector>
 #include "../random_number.h"
 using namespace std;
 
-//template <typename T>
-//void sort( T *arr, int length ) {
-//			
-//}
-
-int getMax( int *arr, int n ) {
-	int max = arr[ 0 ];
-	for( int i = 1; i < n; ++i )
-		if( arr[ i ] > max )
-			max = arr[ i ];
-	return max;
-}
-
-void countSort( int *arr, int n, int exp ) {
-	int outPut[ n ];
-	int i, count[ 10 ] = { 0 };
+namespace space{
+template <typename T>
+T retMax( T *arr, int length ) {
+	T maxValue = arr[ 0 ];
 	
-	for( int i = 0; i < n; ++i ) {
-		count[ ( arr[ i ]/exp )%10 ]++;
-		//cout << "count[ ( arr[ i ]/exp )%10 ]: " << count[ ( arr[ i ]/exp )%10 ] << endl;
-	}
-	cout << "one for" << endl;
-		
-	for( i = 1; i < 10; ++i ) {
-		count[ i ] +=count[ i - 1 ];
-		//cout << "count[ i ] +=count[ i - 1 ]]: " << count[ i ] << endl;
-	}
-	//cout << "two for" << endl;	
-		
-	for( i = n - 1; i >= 0; --i ) {
-		cout << "before outPut[ count[ ( arr[ i ] / exp ) % 10 ] - 1 ]: " << outPut[ count[ ( arr[ i ] / exp ) % 10 ] - 1 ] << endl;
-		outPut[ count[ ( arr[ i ] / exp ) % 10 ] - 1 ] = arr[ i ];
-		cout << "outPut[ count[ ( arr[ i ] / exp ) % 10 ] - 1 ]: " << outPut[ count[ ( arr[ i ] / exp ) % 10 ] - 1 ] << endl;
-		count[ ( arr[ i ] / exp )%10 ]--;
-		//cout << "count[ ( arr[ i ] / exp )%10 ]--: " << count[ ( arr[ i ] / exp )%10 ] << endl;
-	}
-	//cout << "three for" << endl;
+	for( int i = 1; i < length; ++i )
+		if( maxValue < arr[ i ] )
+			maxValue = arr[ i ];
+	return maxValue;
 	
-	for( i = 0; i < n; ++i ) {
-		arr[ i ] = outPut[ i ];
-		//cout << "arr[ i ]" << arr[ i ] << ends;
-	}
-	//cout << "end for" << endl;	
 }
 
-void radixSort( int *arr, int n ) {
-	int m = getMax( arr, n );
-	for( int exp = 1; m/exp > 0; exp *= 10 )
-		countSort( arr, n, exp );
+template <typename T>
+void radixSort( T *arr, int length ) {
+		vector<T>bucke[ 10 ];
+		int maxValue = retMax( arr, length );
+		int exp = 1;
+		while( maxValue > 0 ) {
+			
+			for( int i = 0; i < length; ++i )
+				bucke[ ( arr[ i ] / exp ) %10 ].push_back( arr[ i ] );
+			
+			exp *= 10;
+			maxValue /= 10;
+			
+			for( int buckeIndex = 0, elemIndex = 0, index = 0; 
+					buckeIndex < 10; ++buckeIndex ) {
+				for( int elemIndex = 0; elemIndex < bucke[ buckeIndex ].size(); ++elemIndex )
+					arr[ index++ ] = bucke[ buckeIndex ][ elemIndex ];
+				bucke[ buckeIndex ].clear();
+			}
+		}
+	}	
 }
+
+
 
 
 int main(int argc, char *argv[])
 {
-	int length = 30;
-	int *data = Generate_random_numbers( 20, 100, length );
+	int length = 100;
+	int *data = Generate_random_numbers( 1, 10000, length );
 	
-	for( int i = 0; i < length; ++i )
-		cout << data[ i ] << ends;
-	cout << endl;
 	
-	radixSort( data, length );
-	
-	for( int i = 0; i < length; ++i )
-		cout << data[ i ] << ends;
+	testSort( "space::radixSort", space::radixSort, data, length );
+
 	delete []data;
 	
 	return 0;
--- 数据结构/排序/桶排序.cpp
+++ 数据结构/排序/桶排序.cpp
@@ -14,13 +14,59 @@
 #include "../random_number.h"
 using namespace std;
 
-namespace bucket {
+namespace space {
 
+template<typename T>
+pair<T, T>retMaxANDMin( T *array, int length ) {
+	T max = array[ 0 ];
+	T min = array[ 1 ];
+	
+	for( int i = 1; i < length; ++i ) {
+		if( max < array[ i ] );
+			max = array[ i ];
+		if( min > array[ i ] )
+			min = array[ i ];
+	}
+	return pair<T, T>( max, min );
+}
+template<typename T>
+void bucketSort( T *array, int length ) {
+		vector<T>bucket[ length ];
+		auto pair = retMaxANDMin( array, length );
+		T difference = pair.first - pair.second;
+		
+		for( int i = 0; i < length; ++i ) {
+			int index = ( int )( ( array[ i ] - pair.second ) * ( length - 1 ) / difference );
+			
+			bucket[ index ].push_back( array[ i ] );
+		}
+		
+		for( int i = 0; i < length; ++i )
+			sort( bucket[i].begin(), bucket[ i ].end() );
+		
+		int elemIndex = 0;	
+		for( int i = 0; i < length; ++i )
+			for( int j = 0; j < bucket[ i ].size(); ++j )
+				array[ elemIndex++ ] = bucket[ i ][ j ];
+		
+	}
 }
 
 int main(int argc, char *argv[])
 {
-
+	int length = 100;
+	int *data = Generate_random_numbers( 1, 100, length );
+	cout << "before: " ;
+	for( int i = 0; i < length; ++i )
+		cout << data[ i ] << ends;
 	
+	cout << endl;
+	testSort( "space::bucketSort", space::bucketSort, data, length );
+	
+	cout << "after: ";
+	for( int i = 0; i < length; ++i )
+		cout << data[ i ] << ends;
+	
+	delete []data;
 	return 0;
 }
