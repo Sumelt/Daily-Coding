a5c931332156bd14a4c9f57e198a481337290eea
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-05-16 00:00:01 +0800

    05、15

--- leetcode/71_简化路径.cpp
+++ leetcode/71_简化路径.cpp
@@ -17,38 +17,54 @@ using namespace std;
 
 class Solution {
 private:
-	bool flag;
+	stack<string>stk;
 	
 public:
     string simplifyPath(string path) {
-        string legalPath = string();
-        flag = false;
-  		stack<char>stack;
-		      
 		if( !path.size() )
-        	return legalPath;
-        	
- 		for( int i = 0; i < path.size(); ++i ) {
-		 	if( path[ i ] != '.' ) {
-				if( path[ i ] == '/' && stack.size() > 0 && stack.top() == '/' )
-					continue;
-				else stack.push( path[ i ] );
-	 		}
- 		}
-
-	 	if( stack.top() == '/' && stack.size() > 1)
-	 		stack.pop();
- 		while( !stack.empty() ) {
-		 	legalPath += stack.top();
-		 	stack.pop();
-		 }
-	 	return string( legalPath.rbegin(), legalPath.rend() );
+			return string();
+		string res = string();
+		
+		int idx = 0;
+		if( path[ idx++ ] == '/' )
+			stk.push( "/" );
+			
+		while( idx < path.size() ) {
+			
+			while( idx < path.size() && path[ idx ] == '/' ) 
+				++idx;
+			int start = idx;
+			
+			while( idx < path.size() && path[ idx ] != '/' )
+				++idx;
+				
+			string substr = path.substr( start, idx - start );
+			
+			if( substr.size() > 0  ) {
+				if( substr == ".." )  {
+					if( stk.size() > 1 )
+						stk.pop();					
+				}
+				else if( substr != "." && substr != "/")
+					stk.push( substr );
+			}
+			
+		}
+		while( stk.size() > 1 ) {
+			res.insert( 0, stk.top() );
+			res.insert( 0, "/" );
+			stk.pop();
+		}
+		if( !res.size() )
+			res.insert( 0, "/" );
+			 	
+		return res;
     }
 };
 
 int main(int argc, char *argv[])
 {
 	Solution oj;
-	cout << oj.simplifyPath( "/a//b////c/d//././/.." );
+	cout << oj.simplifyPath("/a/./b/../../c/");
 	return 0;
 }
