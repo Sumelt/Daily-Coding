44a339417311c673c4302ed73ac8e7ccb8343cc1
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-05-26 23:41:49 +0800

    2019/05/26

---
+++ 数据结构/数组/ArrayMain.cpp
@@ -0,0 +1,33 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+#include "./array.h"
+
+int main(int argc, char *argv[])
+{
+	Array<int> ary( 8 );
+	for( int i = 0; i < 8; ++i )
+		ary.push_front( i );
+	ary.remove_back();
+	ary.remove_back();
+	ary.remove_back();
+	ary.remove_back();
+	ary.remove_back();
+	ary.remove_back();
+	ary.print();
+//	ary.set( 2, 900 );
+//	ary.print();
+//	cout << ary.find( 2 );
+//	ary.remove_front();
+//	ary.print();
+//	ary.remove_back();
+//	ary.print();
+	return 0;
+}
---
+++ 数据结构/数组/array.h
@@ -0,0 +1,144 @@
+﻿#ifndef __ARRAY_H__
+#define __ARRAY_H__
+
+#include <iostream>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+template <typename T>
+class Array{
+public:
+	Array( int count ) {
+		capacity = count;
+		array = new T[ capacity ]();
+		size = 0;
+	}
+	Array() {
+		capacity = 10;
+		array = new T[ capacity ]();
+		size = 0;
+	}
+	~Array() {
+		delete []array;
+	}
+	
+	int getSize() {
+		return size;
+	}
+	
+	int getLength() {
+		return capacity;
+	}
+	
+	bool empty() {
+		return size == 0;
+	}
+	
+	void push_back( T element ) {
+		push( size, element );
+	}
+	
+	void push_front( T element ) {
+		push( 0, element );
+	}
+	
+	void push( int index, T element ) {
+		if( size >= capacity )
+			resize( capacity );
+		assert( index >= 0 && index <= size );
+		
+		for( int i = size; i > index; --i )
+			array[ i ] = array[ i - 1 ];
+		array[ index ] = element;
+		++size;
+	}
+	
+	T get( int index ) {
+		assert( index >= 0 && index < size );
+		return array[ index ];
+	}
+	
+	void set( int index, T element ) {
+		assert( index >= 0 && index < size );
+		array[ index ] = element;
+	}
+	
+	bool contain( T element ) {
+		for( int i = 0; i < size; ++i )
+			if( array[ i ] == element )
+				return true;
+		return false;
+	}
+	
+	int find( T element ) {
+		for( int i = 0; i < size; ++i )
+			if( array[ i ] == element )
+				return i;
+		return -1;		
+	}
+	
+	int remove( int index ) {
+		assert( index >= 0 && index < size );
+		T value = array[ index ];
+		for( int i = index; i < size; ++i )
+			array[ i ] = array[ i + 1 ];
+		array[ size - 1 ] = 0;
+		--size;
+		
+		//缩容 
+		if( size == ( getLength() / 4 ) )
+			resize( capacity / 2 );
+		return value;
+	}
+	
+	int remove_front() {
+		return remove( 0 );
+	}
+	
+	int remove_back() {
+		return remove( size - 1 );
+	}
+	
+	void remove_element( T element ) {
+		int index = find( element );
+		assert( index >= 0 && index < size );
+		remove( index );
+	}
+	
+	void print() {
+		cout << "Array: size = " << size << ' ' 
+				<< "capacity = " << capacity << endl;
+				
+		cout << '[';
+		for( int i = 0; i < size; ++i ) {
+			cout << array[ i ];
+			if( i != size - 1 )
+				cout << ", ";
+		}
+		cout << ']'	<< endl;
+	}
+
+private:
+	int capacity;
+	T *array;
+	int size;
+	
+	//扩容 
+	void resize( int newCapacity ) {
+		int oldCapacity = capacity;
+		if( newCapacity > capacity ) {
+			T *ptr = new T[ newCapacity ]();
+			capacity = newCapacity;
+			copy( array, array + oldCapacity, ptr );//拷贝旧元素 
+			std::swap( array, ptr );			
+		}
+		else {
+			capacity = newCapacity;
+			size = oldCapacity / 4;
+		} 
+	}
+};
+
+
+#endif
