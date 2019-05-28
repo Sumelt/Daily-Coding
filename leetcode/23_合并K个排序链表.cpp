ce54621024bd8437d236cd49736f025ebf4bfe93
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-05-28 22:59:53 +0800

    2019-05-28

---
+++ leetcode/23_合并K个排序链表.cpp
@@ -0,0 +1,67 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <queue>
+#include <functional>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool cmp( ListNode* list1, ListNode* list2 ) {
+	return list1->val > list2->val;
+}
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if( lists.size() == 0 )
+        	return nullptr;
+  		ListNode *res = nullptr;
+       	priority_queue<ListNode*, vector<ListNode*>, 
+		   			function<bool( ListNode*, ListNode* )>>que( cmp );
+   		for( ListNode* node = lists[ 0 ]; node != nullptr; node = node->next )
+   			que.push( node );				
+						
+       	for( int i = 1; i < lists.size(); ++i ) {
+       		ListNode* node = lists[ i ];
+			while( !que.empty() && node	!= nullptr ) {
+				ListNode* head = que.top();
+				if( head->val < node->val )
+					head->next = node;
+				else node->next = head;
+				que.pop();
+				node = node->next
+			}
+    	}
+       		
+    }
+};
+
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
---
+++ leetcode/347_前K个高频元素.cpp
@@ -0,0 +1,61 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <cassert>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+		assert( k > 0 );
+		if( nums.size() == 0 )
+			return vector<int>();
+		
+		unordered_map<int, int>mp;//记录每一个元素的出现次数
+		for( int i = 0; i < nums.size(); ++i )
+			++mp[ nums[ i ] ];
+		//构造一个最小堆，STL容器是按照键来排序的，所以把出现次数作为健，数字作为值
+        //仿函数：greater：数字越大优先级越低
+		priority_queue<pair<int, int>, vector<pair<int, int>>, 
+								greater<pair<int, int >>>pque; //count - element
+		for( unordered_map<int, int>::iterator iter = mp.begin(); iter != mp.end(); ++iter ) {
+			if( pque.size() == k ) {
+                //新压入的元素出现的次数比堆顶元素出现的次数还大，更新堆顶
+				if( iter->second > pque.top().first ) {
+					pque.pop();
+					pque.push( make_pair( iter->second, iter->first ) );
+				}
+			}
+			else pque.push( make_pair( iter->second, iter->first ) );
+		}
+		
+		vector<int>res;
+		while( !pque.empty() ) {
+			res.push_back( pque.top().second );
+			pque.pop();
+		}
+		//由于是个最小堆，元素出现次数是按照从小到大，则必须来一个翻转	
+		return vector<int>( res.rbegin(), res.rend() );
+    }
+};
+
+int main(int argc, char *argv[])
+{
+	Solution oj;
+	vector<int>vec = { 1,1,1,2,2,3 };
+	vec = oj.topKFrequent( vec, 2 );
+	for( auto value : vec )
+		cout << value << ' ';
+	return 0;
+}
