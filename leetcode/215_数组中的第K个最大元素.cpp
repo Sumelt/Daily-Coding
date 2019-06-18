2f9bf41d9526013dab5eeb7c9ded50e8d3dd0a34
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-06-19 00:00:02 +0800

    2019-06-18

---
+++ leetcode/215_数组中的第K个最大元素.cpp
@@ -0,0 +1,40 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <queue>
+using namespace std;
+
+class Solution {
+private:
+	priority_queue<int, vector<int>, greater<int>>pque;
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        if( nums.size() == 0 )
+        	return 0;
+       	
+        for( auto value : nums )
+        	if( pque.size() == k && pque.top() < value ) {
+	        	pque.pop();
+	        	pque.push( value );
+	        }
+	        else if( pque.size() < k ) pque.push( value );
+        return pque.top();
+    }
+};
+
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
