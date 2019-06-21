cd225c0cd9ee8e0697806a8526a4fc54881a9ecc
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-06-21 23:06:26 +0800

    2019-06-21

---
+++ 数据结构/Tree/RBTree.h
@@ -0,0 +1,30 @@
+﻿#ifndef __RBTREE__H_
+#define __RBTREE__H_
+
+enum colors = { RED, BLACK };
+
+template <typename Key, typename Value>
+class RBTree {
+private :
+	struct Node {
+		Key key;
+		Value value;
+		Node *left;
+		Node *right;
+		int color;		
+		Node( Key k, Value v ) : 
+			left( nullptr ), right( nullptr ), value( v ), key( k ), color( re )
+			{}
+		Node( Node *node ) : key( node->key ), value( node->value ), 
+			left( node->left ), right( node->right ), height( 1 )
+			{}
+};
+
+public :
+
+
+
+};
+
+
+#endif
---
+++ 数据结构/Tree/RBTreeMain.cpp
@@ -0,0 +1,21 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include "./RBtree.h" 
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
