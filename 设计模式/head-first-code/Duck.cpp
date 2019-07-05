030aeb67155801559342efd1822ae2665c8f7192
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-07-05 23:40:46 +0800

    2019-07-05

---
+++ 设计模式/head-first-code/Duck.cpp
@@ -0,0 +1,19 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <vector>
+#include <cassert>
+using namespace std;
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
