1d2a0737031377b07d2a58117938418d86002922
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-07-07 23:00:51 +0800

    2019-07-07

--- 设计模式/head-first-code/Duck.cpp
+++ 设计模式/head-first-code/Duck.cpp
@@ -12,8 +12,83 @@
 #include <vector>
 #include <cassert>
 using namespace std;
+
+//抽象飞行类 
+class Fly {
+public:
+	virtual void fly() = 0;
+};
+
+//抽象叫声类 
+class Quack {
+public:
+	virtual void quack() = 0;
+};
+
+//抽象基类 
+class Duck {
+public:
+	Fly *flyBehavior;
+	Quack *quackBehavior;
+	
+	virtual ~Duck();
+	void performFly();
+	void performQuack();
+	void performSwim();//所有的鸭子都会叫，属于公有部分 
+};
+
+Duck::~Duck() {
+	delete flyBehavior;
+	delete quackBehavior;
+}
+
+void Duck::performFly() {
+	flyBehavior->fly();
+}
+
+void Duck::performQuack() {
+	quackBehavior->quack();	
+}
+
+void Duck::performSwim() {
+	cout << "all duck can swimming" << endl;
+}
+
+//**********************// 
+//自定义飞行动作类 
+class FlyWithWind : public Fly {
+public:
+	void fly() {
+		cout << "I can Fly" << endl;
+	}
+};
+
+//自定义叫声类 
+class QuackMute : public Quack {
+public:
+	void quack() {
+		cout << "I can quack wawa" << endl;
+	}
+};
+
+//自定义类实现 
+class MuteDuck : public Duck {
+public:
+	MuteDuck();
+};
+
+MuteDuck::MuteDuck() {
+	flyBehavior = new FlyWithWind();
+	quackBehavior = new QuackMute();
+}
+
+
+
 int main(int argc, char *argv[])
 {
-	
+	MuteDuck mduck;
+	mduck.performFly();
+	mduck.performQuack();
+	mduck.performSwim();
 	return 0;
 }
