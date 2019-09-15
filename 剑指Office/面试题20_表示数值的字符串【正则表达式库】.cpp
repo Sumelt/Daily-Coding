121c24f5e3202d54d5ef0da9a6b6aaf304e59496
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-09-15 21:43:12 +0800

    2019-09-15

---
+++ 剑指Office/面试题20_表示数值的字符串【正则表达式库】.cpp
@@ -0,0 +1,32 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date: 
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+class Solution {
+public:
+    bool match( const char* str, const char* pattern ) {
+		std::regex rgx( pattern );
+    	return std::regex_match( str, rgx );
+    }
+};
+
+int main(int argc, char *argv[])
+{
+	const char *pat = "a";
+	const char *str = "";
+	Solution oj;
+	cout << oj.match( pat, pat );
+	return 0;
+}
