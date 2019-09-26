6d8a0e5bbf6c02f8a77609e9d75c3688ff325317
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-09-26 11:35:29 +0800

    2019-09-26

---
+++ 程序员代码面试指南/矩阵的最小和.cpp
@@ -0,0 +1,71 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+//#include<bits/stdc++.h>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+class Solution {
+private:
+	int find( vector<vector<int>>& grid ) {
+		int dp[ grid.size() ][ grid[ 0 ].size() ] = { { 0 } };
+		
+		//第一行
+		dp[ 0 ][ 0 ] = grid[ 0 ][ 0 ];
+		for( int col = 1; col < grid[ 0 ].size(); ++col )
+			dp[ 0 ][ col ] = dp[ 0 ][ col - 1 ] + grid[ 0 ][ col ];
+		//第一列
+		for( int row = 1; row < grid.size(); ++row )
+			dp[ row ][ 0 ] = dp[ row - 1 ][ 0 ] + grid[ row ][ 0 ];
+			
+		for( int row = 1; row < grid.size(); ++row ) {
+			for( int col = 1; col < grid[ 0 ].size(); ++col )
+				dp[ row ][ col ] = \
+					min( dp[ row - 1 ][ col ], dp[ row ][ col - 1 ] ) + grid[ row ][ col ];
+		}
+		return dp[ grid.size() - 1 ][ grid[ 0 ].size() - 1 ];
+	}
+	//空间压缩 
+	int findWithSpaceCompress( vector<vector<int>>& grid ) {
+		int rows = grid.size(), cols = grid[ 0 ].size();
+		int less = min( rows, cols );
+		int more = max( rows, cols );
+		bool standard = rows >= cols ? true : false;//判断是更新行还是列 
+		
+		int dp[ less ] = { 0 };
+		dp[ 0 ] = grid[ 0 ][ 0 ];
+		
+		//计算第一列或者是第一行 
+		for( int i = 1; i < less; ++i ) 
+			dp[ i ] = dp[ i - 1 ] + ( standard ? grid[ 0 ][ i ] : grid[ i ][ 0 ] );
+		
+		for( int i = 1; i < more; ++i ) {
+			dp[ 0 ] = dp[ 0 ] + ( standard ? grid[ i ][ 0 ] : grid[ 0 ][ i ] );
+			for( int k = 1; k < less; ++k ) 
+				dp[ k ] = ( standard ? grid[ i ][ k ] : grid[ k ][ i ] ) + 
+					min( dp[ k ], dp[ k - 1 ] );
+		} 
+		return dp[ less - 1 ];
+	} 
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if( grid.size() == 0 || grid[ 0 ].size() == 0 )
+        	return 0;
+       	return find( grid );
+    }
+};
+
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
---
+++ 程序员代码面试指南/矩阵的走法.cpp
@@ -0,0 +1,47 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+//#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+class Solution {
+private:
+	int find( vector<vector<int>>& grid ) {
+		int dp[ grid.size() ][ grid[ 0 ].size() ] = { { 0 } };
+		//第一行的元素走法都为1
+		for( int col = 0; col < grid[ 0 ].size(); ++col )
+			dp[ 0 ][ col ] = 1;
+		//第一列的元素走法都为1
+		for( int row = 0; row < grid.size(); ++row ) 
+			dp[ row ][ 0 ] = 1;
+			
+		for( int row = 1; row < grid.size(); ++row ) {
+			for( int col = 1; col < grid[ 0 ].size(); ++col )
+				dp[ row ][ col ] = dp[ row - 1 ][ col ] + dp[ row ][ col - 1 ];
+		}
+		return dp[ grid.size() - 1 ][ grid[ 0 ].size() - 1 ];
+	}
+
+public:
+    int pathCount(vector<vector<int>>& grid) {
+        if( grid.size() == 0 || grid[ 0 ].size() == 0 )
+        	return 0;
+       	return find( grid );
+    }
+};
+
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
