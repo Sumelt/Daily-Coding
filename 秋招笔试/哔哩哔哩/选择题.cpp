128e688ae2815ccf1eb13bdbfe9b6a3e8b6c6d97
Sumelt <40728261+Sumelt@users.noreply.github.com>
2019-09-30 23:59:00 +0800

    2019-09-30

---
+++ 秋招笔试/哔哩哔哩/选择题.cpp
@@ -0,0 +1,52 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+//#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+/*
+class Base {
+private:
+public:
+	Base() = default;
+	virtual void out() = 0;
+	 //~Base() = default; 
+	 //virtual ~Base() = default; 
+	virtual	~Base()  {
+	 	cout << "base printf" << endl;
+ 	} 
+};
+
+class derive : public Base {
+public:
+	//virtual ~derive();
+	~derive() = default; 
+	virtual void out()  {
+		cout << "hello world!!" << endl;
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	Base *bptr = new derive();
+	//bptr->~Base();
+	bptr->out();
+	
+	return 0;
+}
+*/
+
+int main() {
+	int ary[ 5 ] = { 1, 2, 3, 4, 5 };
+	cout << ( ary + 2 )[ 1 ] << endl;
+} 
