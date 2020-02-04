871623be43f27fffcad91f321c8095b2a50a7623
Sumelt <40728261+Sumelt@users.noreply.github.com>
2020-02-05 00:17:27 +0800

    2020-02-05

---
+++ 设计模式/工厂模式-Pizza.cpp
@@ -0,0 +1,153 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+using namespace std;
+
+class Pizza;
+class Pizza_a;
+class Pizza_b;
+class Pizza_c;
+/* 
+//-----------------
+class Pizza{
+private:
+
+public:
+	void prepare();
+	void bake();
+	void cut();
+	void box();
+	 
+};
+//---------------披萨种类
+class Pizza_a : public Pizza{
+	
+};
+
+class Pizza_b : public Pizza{
+	
+};
+
+class Pizza_c : public Pizza{
+	
+};
+
+//创建工厂 
+class SamplePizzaStore {
+private:
+	Pizza *m_pizza;
+	
+public:
+	//简单工厂 
+	~SamplePizzaStore() { delete m_pizza; }
+	Pizza &creatPizza( const string &pizzaType ) {
+		if( pizzaType == string( "A" ) ) //依据类型创建pizza 
+			m_pizza = new Pizza_a();
+		//else if 其他的披萨种类创建
+		 
+		return *m_pizza;
+	}
+};
+
+class PizzaStore {
+private:
+	SamplePizzaStore *m_p_factory; //绑定的工厂 
+	Pizza *m_pizza;
+	
+public:
+	PizzaStore( SamplePizzaStore &factory ) : m_p_factory( &factory ){}
+	~PizzaStore() { delete m_pizza; }
+	Pizza &orderPizza( string &pizzaType ) {
+		m_pizza = &m_p_factory->creatPizza( pizzaType ); //创建披萨交由工厂创建
+		
+		//披萨的一系列其他操作
+		return *m_pizza;
+	}
+};
+
+//-------------------
+*/
+
+//产品：披萨基类 
+class Pizza{
+private:
+	string m_name;
+public:
+	Pizza( const string name ) : m_name( name ){}
+	void prepare(){ cout << " prepare " << m_name << " operation "; }
+	void bake() { cout << " bake operation ";}
+	void cut() { cout << " cut operation "; }
+	void box() { cout << " box operation "; }
+	 
+};
+//不同风味的披萨 
+class AStyleCheesePizza : public Pizza {
+public:
+	AStyleCheesePizza() : Pizza( "AStyleCheesePizza" ){}
+};
+
+class BStyleCheesePizza : public Pizza {
+public:
+	BStyleCheesePizza() : Pizza( "BStyleCheesePizza" ){}
+};
+
+//充当抽象基类的：披萨店 
+class PizzaStore {
+private:
+	Pizza *m_pizza;
+	
+public:
+	~PizzaStore() { delete m_pizza; }
+	
+	//create回到基类，具体的实现交由子类实现，延迟实例化 
+	virtual Pizza &creatPizza( const string &pizzaType ) = 0; //纯虚函数：充当工厂接口 
+	Pizza &orderPizza( const string &pizzaType ) {
+		m_pizza = &creatPizza( pizzaType ); //
+		
+		//披萨的一系列其他操作
+		return *m_pizza;
+	}
+};
+//不同风味的披萨店 
+class AStylePizzaStore : public	PizzaStore {
+public:
+	Pizza &creatPizza( const string &pizzaType ) {
+		if( pizzaType == string( "CheesePizza" ) )
+			return *new AStyleCheesePizza();
+		//else if return //其他风味的披萨
+	} 
+
+};
+
+class BStylePizzaStore : public	PizzaStore {
+public:
+	Pizza &creatPizza( const string &pizzaType ) {
+		if( pizzaType == string( "CheesePizza" ) )
+			return *new BStyleCheesePizza();
+		//else if return //其他风味的披萨 
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	PizzaStore *aStore = new AStylePizzaStore( );
+	Pizza* pizza = &aStore->orderPizza( "CheesePizza" );
+	pizza->prepare();
+	pizza->bake();
+	
+	delete aStore;
+	
+	return 0;
+}
---
+++ 设计模式/观察者模式-Weather.cpp
@@ -0,0 +1,132 @@
+﻿/*----------------------------------------------------------------
+
+* @Author: Su
+
+* @Description: 
+
+* @Creath Date:
+
+----------------------------------------------------------------*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+class Subject;
+class Observer;
+
+
+//观察者：抽象基类 
+class Observer {
+private:
+
+public:
+	virtual void upate( float temperature, float humidity, float pressure ) = 0;
+};
+
+//主题：抽象基类 
+class Subject {	
+public:
+	virtual void registerObserver( Observer *o ) = 0;
+	virtual void removeObserver( Observer *o ) = 0;
+	virtual void notifyObserver() = 0;
+	
+};
+//具体主题 
+class WeatherData : public Subject {
+private:
+	vector< Observer*>observers;
+	float m_temperature;//温度 
+	float m_humidity;//湿度 
+	float m_pressure;//压强
+	bool change = false;
+	
+public:
+	WeatherData(){}
+	void registerObserver( Observer *o ) {
+		observers.push_back( o );
+	}
+
+	void removeObserver( Observer *o ) {
+		for( auto iter = observers.begin(); iter != observers.end(); ++iter ) {
+			//地址相等 
+			if( *iter == o ) {
+				*iter = *( observers.end() - 1 ); //用尾部的元素覆盖要删除位置的元素 
+				observers.pop_back();//删除尾部 
+			}			
+		}					
+	}
+
+	void notifyObserver() {
+		for( auto p_value : observers ) {
+				p_value->upate( m_temperature, m_humidity, m_pressure );
+		}		
+	}
+	void measurementChange() {
+		notifyObserver();
+	}
+	//组件信息发生该变 
+	void setMeasurementChange( float temperature, float humidity, float	pressure ) {
+		this->m_temperature = temperature;
+		this->m_humidity = humidity;
+		this->m_pressure = pressure;
+		measurementChange();
+	}
+	
+};
+
+//显示相关的构件 
+class DisplayElement {
+public:
+	virtual void display() = 0;	
+};
+
+//具体观察者 
+class CurrentConditionDisplay : public Observer, public	DisplayElement {
+private:
+	float m_temperature;//温度 
+	float m_humidity;//湿度 
+	float m_pressure;//压强
+	Subject *m_weatherData; //绑定的组件 
+	
+public:
+	CurrentConditionDisplay( Subject *weatherData ) {
+		this->m_weatherData = weatherData;
+		this->m_weatherData->registerObserver( this ); //观察者加入 
+	}
+	
+	//观察者更新自身 
+	void upate( float temperature, float humidity, float pressure ) {
+		this->m_temperature = temperature;
+		this->m_humidity = humidity;
+		display();
+	}
+		
+	void display() {
+		cout << "CurrentCondition: temperature, humidity, " 
+			<< m_temperature << ' ' << m_humidity << endl; 
+	}
+
+};
+
+
+int main(int argc, char *argv[])
+{	
+	WeatherData *weather = new WeatherData(); //启动一个气象站
+	//观察者1 
+	CurrentConditionDisplay *conditionDisplay1 = new CurrentConditionDisplay( weather );
+	//观察者2 
+	CurrentConditionDisplay *conditionDisplay2 = new CurrentConditionDisplay( weather );
+	weather->setMeasurementChange( 30, 22, 100 );//气象数据更新 
+	
+	weather->removeObserver( conditionDisplay1 ); //删除一号观察者 
+	weather->setMeasurementChange( 55, 12, 99 );//气象数据更新 
+	
+ 	delete weather;
+ 	delete conditionDisplay1;
+ 	delete conditionDisplay2;
+ 	
+	return 0;
+}
